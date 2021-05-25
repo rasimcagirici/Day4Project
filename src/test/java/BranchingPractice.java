55844225cc1dfb9c7974ce5d512f3a7f0b9926d5
Rasim Cagirici <rasimcagirici@gmail.com>
2021-05-25 19:23:36 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,11 @@
 public class BranchingPractice {
     public static void main(String[] args) {
 
+        //created new project
+        //enabled version control
+        //added .gitignore with .idea folder, target folder, and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+
     }
 }
